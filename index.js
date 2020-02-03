4f1a89cd70d436ae32954accd9ab640fcf13f3eb
Samuel Deuter <SamuelDeuter@gmail.com>
2020-02-03 14:30:31 -0800

    Some routes

--- index.js
+++ index.js
@@ -3,7 +3,26 @@ const express = require('express');
 const app = express();
 const PORT = 3000;
 
+const course = 'localhost:3001';
+const checkout = 'localhost:3002';
+const feedback = 'localhost:3003';
+
+app.use('/course/', (req, res) => {
+  console.log('hit');
+  res.end();
+});
+
+app.use('/checkout/', (req, res) => {
+  console.log('hit');
+  res.end();
+});
+
+app.use('/feedback/', (req, res) => {
+  console.log('hit');
+  res.end();
+});
+
 app.listen(PORT, () => {
   // eslint-disable-next-line no-console
-  console.log(`sever listening on port ${PORT}`);
+  console.log(`server listening on port ${PORT}`);
 });

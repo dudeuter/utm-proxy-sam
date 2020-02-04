f4d6d8b813612162027f62665fdf9d51f7227447
Samuel Deuter <SamuelDeuter@gmail.com>
2020-02-03 16:48:11 -0800

    deleted git submodule, implemented environment

---
+++ src/index.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import Feedback from '../utm-student-feedback/client/components/Feedback';
+
+ReactDOM.render(<Feedback />, document.getElementById('app'));

373153e1899e24404d6fa9fda940e3bc73604dcf
Samuel Deuter <SamuelDeuter@gmail.com>
2020-02-03 17:21:19 -0800

    npm install feedback component, and link it

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import Feedback from '../utm-student-feedback/client/components/Feedback';
+import Feedback from 'utm-student-feedback/client/components/Feedback';
 
 ReactDOM.render(<Feedback />, document.getElementById('app'));
